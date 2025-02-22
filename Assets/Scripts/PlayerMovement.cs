944cffef077271326072af1f229014e99e1384c5
luvdamuzik <rijadmutapcic2@gmail.com>
2025-02-22 17:26:01 +0100

    ducking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Internal;
 using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 
@@ -15,15 +16,19 @@ public class PlayerMovement : MonoBehaviour
     public float groundCheckCircle;
     private bool isGrounded;
 
+    private bool isDucking;
+    public Collider2D normalCollider;
+    public Collider2D duckCollider;
+
     void Start()
     {
-        // Get the Animator component
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
+
         // Flip sprite based on movement direction
         if (input < 0)
             spriteRenderer.flipX = true;
@@ -34,18 +39,42 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckCircle, groundLayer);
 
         // Jump logic
+        //if (Input.GetButtonDown("Jump") && isGrounded && !isDucking) dont allow jump ducking
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, jumpForce);
         }
 
-        // Set animator parameter for walking
-        animator.SetBool("isWalking", input != 0);
+        // Duck logic
+        //if (Input.GetAxisRaw("Vertical") < 0 && isGrounded) dont allow ducking in air
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            isDucking = true;
+            //animator.SetBool("isDucking", true);
+            normalCollider.enabled = false;
+            duckCollider.enabled = true;
+        }
+        else
+        {
+            isDucking = false;
+            //animator.SetBool("isDucking", false);
+            normalCollider.enabled = true;
+            duckCollider.enabled = false;
+        }
+
+        // Set animator parameter for walking (only if not ducking)
+        animator.SetBool("isWalking", input != 0 && !isDucking);
     }
 
     void FixedUpdate()
     {
-        // Only modify x velocity, keeping y velocity unchanged
+        // Allow movement only if not ducking
+        // if (!isDucking) //dont allow moving when ducking
+        // {
+        //     playerRb.linearVelocity = new Vector2(input * speed, playerRb.linearVelocity.y);
+        // }
+
         playerRb.linearVelocity = new Vector2(input * speed, playerRb.linearVelocity.y);
+
     }
 }
